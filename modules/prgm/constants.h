85ba56a874b3f93ca344661294baadcba062eb63
electropourlaction <electropourlaction@users.noreply.github.com>
2016-11-02 08:34:34 +0100

    Add files via upload

---
+++ modules/prgm/constants.h
@@ -0,0 +1,33 @@
+//  env_tcd.h, created by Staffan Jansson on 20/10/14.
+
+#ifndef aleph_env_tcd_h
+#define aleph_env_tcd_h
+
+#define FR32_MAX 0x7fffffff         //2147483647
+#define FR32_MIN 0x80000000         //-2147483648
+#define FR32_MAX1_2 0x3fffffff
+#define FR32_MAX1_3 0x2aaaaaaa
+#define FR32_MAX1_4 0x1fffffff
+#define FR32_MAX1_6 0x15555555
+#define FR32_MAX1_8 0x0fffffff
+#define FR32_MAX1_16 0x07ffffff
+#define FR32_MAX1_24 0x05555555
+#define FR32_MAX1_48 0x02aaaaaa
+#define FR32_MAX2_3 0x55555555
+#define FR32_MAX5_8 0x4fffffff
+#define FR32_MAX8_9 0x71c71c71
+#define FR32_MAX9_16 0x47ffffff
+#define FR32_MAX23_48 0x3d555555
+
+#define FR32_MAX2 0xffffffff
+#define FR32_MAX3_2 0xbfffffff
+#define FR32_MAX5_2 0x13fffffff
+#define FR32_MAX9_8 0x8fffffff
+#define FR32_MAX11_8 0xafffffff
+
+#define PAD 0x4fffffff
+
+#define AUX_SIZE_1 (AUX_SIZE - 1)
+
+
+#endif
